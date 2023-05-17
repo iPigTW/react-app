9d9e4b36ad83e6c0fea14990eb756ce145db27d7
ipigtw <diegogamingtw@gmail.com>
2023-05-17 21:33:46 +0800

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function Home() {
   return <>
-  <h1>Wassup! I'm iPig</h1>
+  <h1>Hello! I'm iPig👋</h1>
   <h3>I'm a 13 years old programmer from Taiwan 🇹🇼</h3>
   <h2>My skills</h2>
   <img src="https://skillicons.dev/icons?i=js,py,react,lua,discord,flask" />
@@ -16,6 +16,7 @@ function Home() {
   <a href="https://github.com/iPigTW"><img src='/github.png' width="50" height="50"/></a>
   <a href="https://youtube.com/@ipigtaiwan"><img src='/youtube.png' width="50" height="50"/></a>
   <a href="https://discord.gg/4uezbEqfSA"><img src='/discord.png' width="55" height="45"/></a>
+  <p className='footer'>Made with <a href="https://react.dev">React</a> by iPig</p>
   </>
 }
 
