4f5ef1eb8a445a7b5d2434f18f46b6819d586f7e
ipigtw <diegogamingtw@gmail.com>
2023-05-20 22:19:01 +0800

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function Home() {
   <h2>My social medias</h2>
   <a href="https://github.com/iPigTW"><img src='/github.svg' width="50" height="50"/></a>
   <a href="https://youtube.com/@ipigtaiwan"><img className='youtube' src='/youtube.png' width="45" height="45"/></a>
-  <a href="https://discord.gg/4uezbEqfSA"><img src='/discord.svg' width="55" height="45"/></a>
+  <a href="https://discord.gg/DxKYsQNvT8"><img src='/discord.svg' width="55" height="45"/></a>
   <p className='footer'>Made with <a href="https://react.dev">React</a> by iPig</p>
   </>
 }
